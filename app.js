0ce6696e938871bcdf4460d6d877f5e58002e0de
Agustin <agualvila@gmail.com>
2022-01-18 16:55:06 -0300

    add boolean

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-function add(n1, n2, phrase) {
-    return phrase + "".concat(n1 + n2);
+function add(n1, n2, phrase, show) {
+    if (show) {
+        return phrase + "".concat(n1 + n2);
+    }
+    else {
+        return 'Result disabled!';
+    }
 }
 var number1;
 var number2;
 var resultPhrase;
+var showResult;
 number1 = 3;
 number2 = 5;
 resultPhrase = 'The "add" result is: ';
-var result = add(number1, number1, resultPhrase);
+showResult = true;
+var result = add(number1, number1, resultPhrase, showResult);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
-function add(n1: number, n2: number, phrase: string) {
-  return phrase + `${n1 + n2}`;
+function add(n1: number, n2: number, phrase: string, show: boolean) {
+  if (show) {
+    return phrase + `${n1 + n2}`;
+  } else {
+    return 'Result disabled!';
+  }
 }
 
 let number1: number;
 let number2: number;
 let resultPhrase: string;
+let showResult: boolean;
 
 number1 = 3;
 number2 = 5;
 resultPhrase = 'The "add" result is: ';
+showResult = true;
 
-const result = add(number1, number1, resultPhrase);
+const result = add(number1, number1, resultPhrase, showResult);
 console.log(result);
